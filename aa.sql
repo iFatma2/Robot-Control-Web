1d46cf5e4254d28a27a130bb3606c7a8e30e94bf
iFatma2 <139279448+iFatma2@users.noreply.github.com>
2023-07-15 21:51:13 +0300

    Add files via upload

---
+++ aa.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 14, 2023 at 09:30 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `direction`
+--
+
+CREATE TABLE `direction` (
+  `direct` varchar(30) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `direction`
+--
+
+INSERT INTO `direction` (`direct`, `id`) VALUES
+('r', 1),
+('f', 2),
+('r', 3),
+('r', 4),
+('f', 5),
+('r', 6),
+('l', 7),
+('b', 8);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `direction`
+--
+ALTER TABLE `direction`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `direction`
+--
+ALTER TABLE `direction`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
